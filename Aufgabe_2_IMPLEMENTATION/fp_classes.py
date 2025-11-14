6805d989bc442544cbd8c4625c96d74ffc97eff8
dizzleshizzle <109990632+dizzleshizzle@users.noreply.github.com>
2025-11-14 21:16:44 +0100

    A2 adjust_epsilon

---
+++ Aufgabe_2_IMPLEMENTATION/fp_classes.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+class environment:
+	def __init__(self):
+		self.N_states = 100
+		self.target_position = 8
+		self.starting_position = 30
+		
+		self.obstacle_interval = np.arange(9,12)
+		self.P_obstacle = 0.0
+	
+class agent:
+	def __init__(self,env_):
+		self.N_episodes = 10000
+		self.tmax_MSD = 100
+		
+		self.x = 1
+		self.Q = np.zeros((env_.N_states,3))
+		self.alpha = 0.01
+		self.gamma = 0.9
+		self.epsilon = 1.0
+		self.target_reward = 10.0
+		self.zero_fraction = 0.9
+
+		self.D = 0.125
+		self.P_diffstep = 2*self.D
+		
+		self.x_old = None
+		
+		if self.P_diffstep > 1.0:
+			print(f"self.P_step = {self.P_step} > 1.0 in agent.__init__(...)")
+			print("Diffusion constant self.D possibly too large. Pick a smaller self.D.")
+			exit()
+	
+	def random_step(self):
+		if np.random.rand()<self.P_diffstep:
+			self.x+=2*np.random.randint(0,2) -1
+		
+	def adjust_epsilon(self,episode):
+		if (episode < self.zero_fraction*self.N_episodesi-1):
+			self.epsilon -= 1/(self.zero_fraction*self.N_episodes)
+		else:
+			self.epsilon = 0
+	
+	def choose_action(self):
+		"""
+		wählt eine Zufallsaktion aus mit Wahrscheinlichkeit self.epsilon oder falls zwei Aktionen die höchsten Q-Werte haben.
+		Andernfalls wird der höchste Wert in der jeweiligen Zeile ausgewählt.
+		"""
+		pass
+	def perform_action(self,env_):
+		"""
+		Hier werden die Aktionen ausgeführt. Der Index der Aktion entspricht der Verschiebung auf der x-Achse + 1
+		"""
+
+		pass
+	
+	def update_Q(self,env_):
+		"""
+		Hier werden die Werte der Q-Matrix nach jeder Aktion entsprechend aktualisiert
+		"""
+		pass
+	
+	def stoch_obstacle(self,env_):
+		pass
+
